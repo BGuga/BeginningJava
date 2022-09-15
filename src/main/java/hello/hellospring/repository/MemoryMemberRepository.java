265dfcae5be58685f1d4da99aab5f58ab8de1c1e
Guga <103228463+BGuga@users.noreply.github.com>
2022-09-15 19:41:32 +0900

    간단한 회원 등록과 목록 출력

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,9 +1,10 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.*;
-
 public class MemoryMemberRepository implements MemberRepository{
 
     private static Map<Long, Member> store= new HashMap<>();
